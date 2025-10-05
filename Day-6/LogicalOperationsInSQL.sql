255d90c3bd4e55bfcdb7f1eb98b9838c45170b5e
firrushaik <shaikfirdose7799@gmail.com>
2025-10-05 14:38:29 +0530

    Relational operators in SQL

---
+++ Day-6/LogicalOperationsInSQL.sql
@@ -0,0 +1,43 @@
+Relational Operator in sql:
+
+The Relational operator in sql are classified into many ways they are listed below:
+
+>                Greater than
+<                Less than
+>=               Greater than or equal to
+<=               Lessthan or equal to
+=                Equal to
+!= (<>)          Not equal to
+
+* (=)
+
+SELECT * FROM mysql_learning.product
+WHERE rating=4;
+
+* (!=)
+
+SELECT * FROM mysql_learning.product
+WHERE rating!=4;
+
+* (>)
+
+SELECT * FROM mysql_learning.product
+WHERE price>200;
+
+* (>=)
+
+SELECT * FROM mysql_learning.product
+WHERE price>=200;
+
+
+* (<)
+
+SELECT * FROM mysql_learning.product
+WHERE rating<4;
+
+
+* (<=)
+
+SELECT * FROM mysql_learning.product
+WHERE rating<=4;
+
