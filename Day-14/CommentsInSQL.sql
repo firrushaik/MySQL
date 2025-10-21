9ba5dbbc4347d81022eee6a122c4060f3a58298c
firrushaik <shaikfirdose7799@gmail.com>
2025-10-21 11:18:40 +0530

    SQL Comments

---
+++ Day-14/CommentsInSQL.sql
@@ -0,0 +1,31 @@
+SQL Comments:
+
+Comments are used to explain sections of SQL statements, or to prevent execution of SQL statements.
+
+Note: Comments are not supported in Microsoft Access databases!
+
+Single Line Comments:
+
+Single line comments start with --.
+
+Any text between -- and the end of the line will be ignored (will not be executed).
+
+Example:
+
+SELECT * FROM Customers -- WHERE City='Berlin';
+
+
+
+2) Multi-line Comments:
+
+Multi-line comments start with /* and end with */.
+
+Any text between /* and */ will be ignored.
+
+The following example uses a multi-line comment as an explanation:
+
+Example
+/*Select all the columns
+of all the records
+in the Customers table:*/
+SELECT * FROM Customers;
