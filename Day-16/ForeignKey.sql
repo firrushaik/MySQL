741b68f83af953cbfec5e098b241263ffcdc0c33
firrushaik <shaikfirdose7799@gmail.com>
2025-10-22 23:14:54 +0530

    Foreign key example

---
+++ Day-16/ForeignKey.sql
@@ -0,0 +1,15 @@
+FOREIGN KEY: 
+
+The FOREIGN KEY constraint is a key used to link two tables together.
+
+A FOREIGN KEY is a field (or collection of fields) in one table that refers to the PRIMARY KEY in another table.
+
+Example:
+
+CREATE TABLE Orders (
+    OrderID int NOT NULL,
+    OrderNumber int NOT NULL,
+    PersonID int,
+    PRIMARY KEY (OrderID),
+    FOREIGN KEY (PersonID) REFERENCES Persons(PersonID)
+);
