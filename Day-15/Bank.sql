1b1df6279e22d0fc76a9a76c6600acdfac1a966b
firrushaik <shaikfirdose7799@gmail.com>
2026-02-07 20:37:33 +0530

    Day-15 programs uploaded

--- Day-15/Bank.sql
+++ Day-15/Bank.sql
@@ -12,14 +12,4 @@ bankName VARCHAR(100) DEFAULT "SBI"
 
 
 
-CREATE TABLE mysql_learning.bank1
-(
-accountNumber INTEGER Primary key,
-IFSC VARCHAR(200) Not null,
-name VARCHAR(200) Not null,
-balance INTEGER CHECK (balance>=1000),
-bankName VARCHAR(100) DEFAULT "SBI"
-);
-
-
 
