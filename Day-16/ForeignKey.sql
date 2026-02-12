62cf012e212e87e81b117ece9c006e6755677429
firrushaik <shaikfirdose7799@gmail.com>
2026-02-12 22:29:45 +0530

    Day-16 programs uploaded

--- Day-16/ForeignKey.sql
+++ Day-16/ForeignKey.sql
@@ -15,4 +15,10 @@ CREATE TABLE Orders (
 );
 
 
-guuvvhgynbbh
+CREATE TABLE Orders (
+    OrderID int NOT NULL,
+    OrderNumber int NOT NULL,
+    PersonID int,
+    PRIMARY KEY (OrderID),
+    FOREIGN KEY (PersonID) REFERENCES Persons(PersonID)
+);
