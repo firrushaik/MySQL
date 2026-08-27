9a9ab5b8d76ebe42ae1eba480cb51a961c6ca381
firrushaik <shaikfirdose7799@gmail.com>
2026-08-27 21:41:55 +0530

    Day-15 programs uploaded

--- Day-15/Bank.sql
+++ Day-15/Bank.sql
@@ -8,6 +8,14 @@ Creating bank table:
 
 
 
+CREATE TABLE mysql_learning.bank1
+(
+accountNumber INTEGER Primary key,
+IFSC VARCHAR(200) Not null,
+name VARCHAR(200) Not null,
+balance INTEGER CHECK (balance>=1000),
+bankName VARCHAR(100) DEFAULT "SBI"
+);
 
 
 
@@ -21,6 +29,14 @@ Creating bank table:
 
 
 
+CREATE TABLE mysql_learning.bank1
+(
+accountNumber INTEGER Primary key,
+IFSC VARCHAR(200) Not null,
+name VARCHAR(200) Not null,
+balance INTEGER CHECK (balance>=1000),
+bankName VARCHAR(100) DEFAULT "SBI"
+);
 
 
 
